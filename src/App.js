a1389fe9394eb0026bb39ab3ade39b2f6c698311
Prajwal Haniya <prajwalhaniya18@gmail.com>
2021-09-26 19:31:27 +0530

    expense app

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2> Expense tracking application</h2>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
